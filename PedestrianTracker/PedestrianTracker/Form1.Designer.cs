8107df4ab4b70cf483fba865bbed82154579ed84
Christopher Zumberge <chriszumberge@gmail.com>
2017-12-18 13:58:16 -0500

    Initial code commit

--- PedestrianTracker/PedestrianTracker/Form1.Designer.cs
+++ PedestrianTracker/PedestrianTracker/Form1.Designer.cs
@@ -28,20 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.panAndZoomPictureBox1 = new Emgu.CV.UI.PanAndZoomPictureBox();
+            this.imageDisplay = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.panAndZoomPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageDisplay)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panAndZoomPictureBox1
+            // 
+            this.panAndZoomPictureBox1.Location = new System.Drawing.Point(158, 155);
+            this.panAndZoomPictureBox1.Name = "panAndZoomPictureBox1";
+            this.panAndZoomPictureBox1.Size = new System.Drawing.Size(8, 8);
+            this.panAndZoomPictureBox1.TabIndex = 0;
+            this.panAndZoomPictureBox1.TabStop = false;
+            // 
+            // imageDisplay
+            // 
+            this.imageDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageDisplay.Location = new System.Drawing.Point(0, 0);
+            this.imageDisplay.Name = "imageDisplay";
+            this.imageDisplay.Size = new System.Drawing.Size(944, 493);
+            this.imageDisplay.TabIndex = 2;
+            this.imageDisplay.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(944, 493);
+            this.Controls.Add(this.imageDisplay);
+            this.Controls.Add(this.panAndZoomPictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.panAndZoomPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageDisplay)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Emgu.CV.UI.PanAndZoomPictureBox panAndZoomPictureBox1;
+        private Emgu.CV.UI.ImageBox imageDisplay;
     }
 }
 
--- PedestrianTracker/PedestrianTracker/Form1.cs
+++ PedestrianTracker/PedestrianTracker/Form1.cs
@@ -1,20 +1,100 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using Emgu.CV.Features2D;
+using Emgu.CV.Flann;
+using Emgu.CV.UI;
+using System.Collections;
 
 namespace PedestrianTracker
 {
     public partial class Form1 : Form
     {
+        static VideoCapture _camera;
+
         public Form1()
         {
             InitializeComponent();
+
+            string captureSource = @"girl_walking.mp4";
+            //string captureSource = @"https://youtu.be/3FXUw98rrUY/?action=stream?dummy=param.mjpg";
+
+            try
+            {
+                _camera = new VideoCapture(captureSource);
+                
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+
+            // Occurs when the application finishes processing and is about to enter the idle state
+            Application.Idle += Application_Idle;
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            Mat frame = _camera.QueryFrame();
+
+            if (frame != null)
+            {
+                CvInvoke.Resize(frame, frame, new Size(944, 493));
+
+                long processingTime;
+                Rectangle[] results;
+
+                using (UMat uImage = frame.GetUMat(AccessType.ReadWrite))
+                {
+                    results = FindPedestrian(uImage, out processingTime);
+                }
+
+                foreach (Rectangle rect in results)
+                {
+                    CvInvoke.Rectangle(frame, rect, new Bgr(Color.Red).MCvScalar);
+                }
+
+                imageDisplay.Image = frame;
+            }
+            else
+            {
+                imageDisplay.Image = frame;
+            }
+        }
+
+        private static Rectangle[] FindPedestrian(IInputArray image, out long processTime)
+        {
+            Stopwatch watch;
+            Rectangle[] regions;
+
+            using (InputArray iaImage = image.GetInputArray())
+            {
+                using (HOGDescriptor des = new HOGDescriptor())
+                {
+                    des.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());
+                    watch = Stopwatch.StartNew();
+
+                    MCvObjectDetection[] results = des.DetectMultiScale(image);
+                    regions = new Rectangle[results.Length];
+
+                    for (int i = 0; i < results.Length; i++)
+                    {
+                        regions[i] = results[i].Rect;
+                    }
+                    watch.Stop();
+                }
+            }
+
+            processTime = watch.ElapsedMilliseconds;
+
+            return regions;
         }
     }
 }
